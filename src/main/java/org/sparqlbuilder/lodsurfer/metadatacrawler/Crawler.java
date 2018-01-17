30aff65ef7529608d0e073a5fca07bd6f1479b3e
nori <nori@10.3.3.129>
2018-01-17 23:31:41 +0900

    カテゴリ判定ロジックの修正

---
+++ src/main/java/org/sparqlbuilder/lodsurfer/metadatacrawler/Crawler.java
@@ -0,0 +1,37 @@
+package org.sparqlbuilder.lodsurfer.metadatacrawler;
+
+import java.net.URI;
+import java.util.HashSet;
+
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.Resource;
+
+
+
+public interface Crawler {
+	
+	public URI endpointURI = null;
+	
+	public URI[] getGraphURIs() throws Exception;
+	public void crawl(URI[] graphURIs) throws Exception;
+
+	
+	
+	
+	public SchemaCategory determineSchemaCategory(Model model, Resource datasetResource) throws Exception;
+
+	public Schema getClassSchema(Schema schema, URI[] classURIs) throws Exception;
+	
+	public Schema getPropertySchema(Model model, Resource datasetRes, URI[] propertyURIs, HashSet<String> datatypeSet) throws Exception;
+	
+
+	public Model getPropertiesFromInstanceDecls() throws Exception;
+	
+	public Model getProperiesFromDomainRangeDecls() throws Exception;
+	public URI[] getDeclaredRDFsClasses() throws Exception;
+//	public String[] getInferedRDFsClassesFromInstances() throws Exception;
+	public URI[] getRDFProperties(URI[] graphURIs) throws Exception;
+	public String[] getDatatypes(URI[] propertyURIs) throws Exception;
+
+
+}
